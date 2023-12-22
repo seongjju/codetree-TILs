c999b24ea09b6cc77186acfe735563322740fde4
seongju Jeong <95429101+seongjju@users.noreply.github.com>
2023-12-22 16:17:40 +0900

    Title: 최소공배수 구하기, Time: 116ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 231222/최소공배수 구하기/find-the-least-common-multiple.py
@@ -0,0 +1,11 @@
+a,b = map(int,input().split())
+lists=[]
+for i in range(1+max(a,b),a*b):
+    if i%a == 0 and i%b ==0:
+        lists.append(i)
+
+re =10000000000
+for result in lists:
+    if re>result:
+        re = result
+print(re)

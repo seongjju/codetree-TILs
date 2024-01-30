afa5989fa31366cb981ade808cd6c0647e338818
seongju Jeong <95429101+seongjju@users.noreply.github.com>
2024-01-30 12:40:18 +0900

    Title: 함수를 이용한 소수 판별, Time: 124ms, Memory: 24MB, Status: Passed - CodeTree

--- 240130/함수를 이용한 소수 판별/decimal-decisions-using-functions.py
+++ 240130/함수를 이용한 소수 판별/decimal-decisions-using-functions.py
@@ -1,14 +1,17 @@
 a,b=map(int,input().split())
 
 def cal(x,y):
-    sums=0
-    for n in range(x,y+1):
-        cnt=0
-        for i in range(2,n):
-            if n%i==0:
-                cnt+=1
-        if cnt==0:
-            sums+=n
-    print(sums)
+    if y==1:
+        print(0)
+    else:
+        sums=0
+        for n in range(x,y+1):
+            cnt=0
+            for i in range(2,n):
+                if n%i==0:
+                    cnt+=1
+            if cnt==0:
+                sums+=n
+        print(sums)
 
 cal(a,b)

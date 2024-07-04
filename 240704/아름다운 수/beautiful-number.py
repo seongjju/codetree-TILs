8d8fa93388d58111f2ea7782b204c75836859ef7
seongju Jeong <95429101+seongjju@users.noreply.github.com>
2024-07-04 21:27:29 +0900

    Title: 아름다운 수, Time: 109ms, Memory: 28MB, Status: Passed - CodeTree

--- 240704/아름다운 수/beautiful-number.py
+++ 240704/아름다운 수/beautiful-number.py
@@ -1,9 +1,16 @@
-n=int(input())
-
-def fun(n):
-    if n==1:
+def count_beautiful_numbers(n, current_length=0):
+    if current_length == n:
         return 1
-    else:
-        return fun(n-1)+n-1
 
-print(fun(n))
+    count = 0
+    for i in range(1, 5):
+        if current_length + i <= n:
+            count += count_beautiful_numbers(n, current_length + i)
+    
+    return count
+
+# 입력 받기
+n = int(input())
+
+# 결과 출력
+print(count_beautiful_numbers(n))

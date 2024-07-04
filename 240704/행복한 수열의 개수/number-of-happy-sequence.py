319ce39c7e487667e311b8f67d8575c9bd06479e
seongju Jeong <95429101+seongjju@users.noreply.github.com>
2024-07-04 17:22:49 +0900

    Title: 행복한 수열의 개수, Time: 121ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240704/행복한 수열의 개수/number-of-happy-sequence.py
@@ -0,0 +1,34 @@
+a,b=map(int,input().split())
+arr=[list(map(int,input().split())) for _ in range(a)]
+
+
+def fun(a,b,arr):
+    cnt=0
+    for r in range(0,a):
+        result = 0
+        for c in range(1,a):
+            if arr[r][c] == arr[r][c-1]:
+                result +=1
+            else:
+                if result >= b:
+                    cnt+=1
+                result = 1
+        if result >= b:
+            cnt+=1
+
+    for c in range(0,a):
+        result = 0
+        for r in range(1,a):
+            if arr[r][c] == arr[r-1][c]:
+                result +=1
+            else:
+                if result >= b:
+                    cnt+=1
+                result = 1
+        if result >= b:
+            cnt+=1
+
+    return cnt
+
+re = fun(a,b,arr)
+print(re)

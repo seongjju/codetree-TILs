7d18e7121bc0d6d4d8bf66ca5cb104ac6fa77a36
seongju Jeong <95429101+seongjju@users.noreply.github.com>
2024-07-04 16:15:24 +0900

    Title: 부분 수열의 합이 m, Time: 118ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240704/부분 수열의 합이 m/the-sum-of-the-subsequences-is-m.py
@@ -0,0 +1,16 @@
+n, m = map(int, input().split())
+arr = list(map(int, input().split()))
+
+dp = [9999999] * (m + 1)
+dp[0] = 0
+
+for num in arr:
+    for j in range(m, num - 1, -1):
+        if dp[j - num] != 9999999:  
+            dp[j] = min(dp[j], dp[j - num] + 1)
+
+
+if dp[m] == 9999999:
+    print(-1)
+else:
+    print(dp[m])

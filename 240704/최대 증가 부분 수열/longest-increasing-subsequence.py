129e989037ffb6fa9af0b98962323444cae99894
seongju Jeong <95429101+seongjju@users.noreply.github.com>
2024-07-04 12:50:17 +0900

    Title: 최대 증가 부분 수열, Time: 159ms, Memory: 32MB, Status: Wrong Answer - CodeTree

--- 240704/최대 증가 부분 수열/longest-increasing-subsequence.py
+++ 240704/최대 증가 부분 수열/longest-increasing-subsequence.py
@@ -1,15 +1,19 @@
 n=int(input())    
 arr=list(map(int,input().split()))
-arr2=[-1] * n
-arr2[0] = arr[0]
-for j in range(1,n):
-     arr2[j]=max(arr[j],arr2[j-1])
+arr2=  [[-1]*n for _ in range(n)]
+arr2[0][0] = arr[0]
 
-cnt=0
-x=[]
-for i in arr2:
-    if i not in x:
-        x.append(i)
-        cnt+=1
+for i in range(0,n):
+    for j in range(i,n):
+        arr2[i][j]=max(arr[j],arr2[i][j-1])
 
-print(cnt)
+li=[]
+for i in range(0,n):
+    cnt=0
+    x=[]
+    for j in range(0,n):
+        if arr2[i][j] != -1 and arr2[i][j] not in x:
+            x.append(arr2[i][j])
+            cnt+=1
+    li.append(cnt)
+print(max(li))

7339349af194bc149c76763314b5d8fb255143ac
seongju Jeong <95429101+seongjju@users.noreply.github.com>
2024-07-05 15:44:58 +0900

    Title: 방향에 맞춰 이동, Time: 151ms, Memory: 24MB, Status: Passed - CodeTree

--- 240705/방향에 맞춰 이동/move-in-direction.py
+++ 240705/방향에 맞춰 이동/move-in-direction.py
@@ -1,37 +1,20 @@
 n=int(input())
-
+x,y=0,0
+dx = [1, -1,  0, 0]
+dy = [0,  0, -1, 1]
 for i in range(n):
-    direction,dis = map(int,input().split())
+    data =input().split()
+    direction,dis = data[0], int(data[1])
+    if direction == 'E':
+        move_dir = 0
+    elif direction == 'W':
+        move_dir = 1
+    elif direction == 'S':
+        move_dir = 2
+    else:
+        move_dir = 3
 
-if direction =='W':
-    nx,ny = x-1,y
+    x += dx[move_dir] * dis
+    y += dy[move_dir] * dis
 
-elif direction =='S':
-    nx,ny = x,y-1
-
-
-elif direction =='N':
-    nx,ny = x,y+1
- 
-else:
-    nx,ny = x+1,y
- 
-
-dir_num = 2 # 주어진 방향이 서쪽인 경우
-x, y = 1, 5 # 현재 위치가 (1, 5)인 경우
-
-if dir_num == 0:
-    nx, ny = x + 1, y
-elif dir_num == 1:
-    nx, ny = x, y - 1
-elif dir_num == 2:
-    nx, ny = x - 1, y
-else:
-    nx, ny = x, y + 1
-
-
-dir_num = 2 
-x, y = 1, 5
-dx, dy = [1, 0, -1, 0], [0, -1, 0, 1]
-
-nx, ny = x + dx[dir_num], y + dy[dir_num]
+print(x,y)

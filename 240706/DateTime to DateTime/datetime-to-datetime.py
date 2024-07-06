83fee1cf0b5c02a19f9ac316a44d5b11435ae419
seongju Jeong <95429101+seongjju@users.noreply.github.com>
2024-07-06 15:36:43 +0900

    Title: DateTime to DateTime, Time: 5095ms, Memory: 24MB, Status: Time Limit Exceed - CodeTree

---
+++ 240706/DateTime to DateTime/datetime-to-datetime.py
@@ -0,0 +1,24 @@
+#일 시 분
+a,b,c = map(int,input().split())
+
+#기준
+sa,sb,sc = 11,11,11
+# arr = [0,31,28,31,30,31,50,31,31,30,31,30,31]
+
+cnt= 0
+
+while True:
+    if sa == a and sb == b and sc == c:
+        break
+        
+    cnt +=1
+    sc+=1
+
+    if sc == 60:
+        sc=0
+        sb+=1
+    if sb == 24:
+        sb=0
+        sa+=1
+
+print(cnt)

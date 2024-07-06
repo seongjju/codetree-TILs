782ec03cb5d672340c3f33776b5ff1d34bcf2150
seongju Jeong <95429101+seongjju@users.noreply.github.com>
2024-07-06 15:56:27 +0900

    Title: 요일 맞추기, Time: 101ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240706/요일 맞추기/guess-day-of-week.py
@@ -0,0 +1,31 @@
+a,b,c,d = map(int,input().split())
+
+arr = [0,31,28,31,30,31,30,31,31,30,31,30,31]
+day = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+cnt= 0
+#5월 4일 기준
+#5월 5일
+#6월 3일
+#6월 5일
+if (a<c) or (a==c and b<=d):
+
+    while True:
+        if a==c and b== d:
+            break
+        cnt +=1
+        b+=1
+        if b==arr[a]:
+            a+=1 
+            b=0
+    print(day[cnt%7 + 1])
+
+else:
+    while True:
+        if a==c and b== d:
+            break
+        cnt +=1
+        d+=1
+        if d==arr[c]:
+            c+=1 
+            d=0    
+print(day[cnt%7 - 1])

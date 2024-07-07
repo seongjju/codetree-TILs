35b7af1f245be00da6853653e1cc4f81b3fd35a3
seongju Jeong <95429101+seongjju@users.noreply.github.com>
2024-07-07 22:43:45 +0900

    Title: 진수 to 진수, Time: 118ms, Memory: 24MB, Status: Passed - CodeTree

--- 240707/진수 to 진수/transformation-of-number-system.py
+++ 240707/진수 to 진수/transformation-of-number-system.py
@@ -11,19 +11,13 @@ for i in range(len(n)):
 
 # 9->1001
 li=[]
+
 while True:
-    if num <b:
+    if num < b:
         li.append(num)
-        break
-    for i in range(b):
-        if num ==1:
-            break
-        if num%b ==i+1:
-            li.append(i+1)
-            num= num//b
-        else:
-            li.append(0)
-            num = num//b
-            
+        break   # num이 b보다 작으면 변환 완료, while문을 종료
+    
+    li.append(num % b)   # 나머지를 리스트에 추가
+    num = num // b   # num을 b로 나눈 몫으로 업데이트
 for i in li[::-1]:
     print(i, end='')

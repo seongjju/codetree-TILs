d8593e92f00a6210f061f94e23fb7ab596ba83fd
seongju Jeong <95429101+seongjju@users.noreply.github.com>
2024-07-07 15:45:20 +0900

    Title: 요일 맞추기, Time: 190ms, Memory: 24MB, Status: Passed - CodeTree

--- 240707/요일 맞추기/guess-day-of-week.py
+++ 240707/요일 맞추기/guess-day-of-week.py
@@ -1,7 +1,7 @@
 a,b,c,d = map(int,input().split())
 
 arr = [0,31,28,31,30,31,30,31,31,30,31,30,31]
-day = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+day = [ 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat','Sun']
 cnt= 0
 
 if (a<c) or (a==c and b<=d):
@@ -9,12 +9,12 @@ if (a<c) or (a==c and b<=d):
     while True:
         if a==c and b== d:
             break
-        cnt +=1
-        b+=1
         if b==arr[a]:
             a+=1 
             b=0
-    print(day[cnt%7 + 1])
+        cnt +=1
+        b+=1
+    print(day[cnt%7 ])
 #다음 년도 처리
 else:
     while True:
@@ -26,6 +26,6 @@ else:
             c+=1 
             d=0    
     reversed_day = day[::-1]
-    print(reversed_day[cnt%7 -2 ])
+    print(reversed_day[cnt%7-1])
 
     # print(day[cnt%7 - 1])

68033090a2d87156b6b9b40237a2a661c2e48ffb
seongju Jeong <95429101+seongjju@users.noreply.github.com>
2024-07-08 00:24:10 +0900

    Title: 왔다 갔던 구역 2, Time: 102ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240708/왔다 갔던 구역 2/area-been-to-and-from2.py
+++ 240708/왔다 갔던 구역 2/area-been-to-and-from2.py
@@ -1,23 +1,23 @@
-n=int(input())
-arr=[0]*200
-x=0
-x=100 #현재
-# arr[100] =1
-for i in range(n):
-    a,b = input().split()
-    a=int(a)
-    if b =='R':
-        for i in range(x+1,a+x+2):
-            arr[x] +=1
-            x+=1
-        x-=1
-    if b == 'L':
-        for i in range(x,a+x+1):
-            arr[x] +=1
-            x-=1     
-        x+=1 
-cnt=0
-for i in range(len(arr)-1):
-    if arr[i]>=2 and arr[i+1]>=2:
-        cnt+=1
+n = int(input())
+arr = [0] * 200  # 배열 크기를 넉넉하게 설정
+x = 100  # 시작 위치를 배열 중앙으로 설정
+
+for _ in range(n):
+    a, b = input().split()
+    a = int(a)
+    if b == 'R':
+        for i in range(a):
+            arr[x] += 1
+            x += 1
+    elif b == 'L':
+        for i in range(a):
+            arr[x] += 1
+            x -= 1
+    arr[x] += 1  # 마지막 위치에서도 증가
+
+cnt = 0
+for i in range(len(arr) - 1):
+    if arr[i] >= 2 and arr[i + 1] >= 2:
+        cnt += 1
+
 print(cnt)

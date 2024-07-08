20fea14dd7f21f1672498f12d11849ada8587906
seongju Jeong <95429101+seongjju@users.noreply.github.com>
2024-07-08 16:07:37 +0900

    Title: 사각형의 총 넓이 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240708/사각형의 총 넓이 2/total-width-of-a-rectangle2.py
@@ -0,0 +1,15 @@
+n= int(input())
+arr = [[0]*200 for _ in range(200)]
+for _ in range(n):
+    x1,y1,x2,y2 = map(int,input().split())
+    for i in range(x1+1+100,x2+1+100):
+        for j in range(y1+1+100,y2+1+100):
+            arr[i][j]= 1
+
+cnt=0
+for i in range(len(arr)):
+    for j in arr[i]:
+        if j==1:
+            cnt+=1
+
+print(cnt)

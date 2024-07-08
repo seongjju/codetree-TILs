fe8408a9d4efd0410fe0c5a86c647c1d4cddc587
seongju Jeong <95429101+seongjju@users.noreply.github.com>
2024-07-08 18:53:57 +0900

    Title: 연속되는 수 2, Time: 114ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240708/연속되는 수 2/continuous-number2.py
+++ 240708/연속되는 수 2/continuous-number2.py
@@ -1,8 +1,10 @@
 n=int(input())
 a=[0] * 1001
 cnt=0
+arr=[]
 for i in range(n):
     a[i] = int(input())
     if i == 0 or a[i] == a[i - 1]:
         cnt += 1
-print(cnt)
+    arr.append(cnt)
+print(max(arr))
